9a85d115e371d6df0dc5c401227ac485ee277ca9
hyunjialee <hyjialee@gmail.com>
2023-11-27 11:12:13 -0500

    test cases for storage of animals

--- src/test/java/com/zipcodewilmington/froilansfarm/Utilities/StorageTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Utilities/StorageTest.java
@@ -1,4 +1,40 @@
 package com.zipcodewilmington.froilansfarm.Utilities;
 
+import com.zipcodewilmington.froilansfarm.Animals.Chicken;
+import com.zipcodewilmington.froilansfarm.Animals.Farmer;
+import com.zipcodewilmington.froilansfarm.Animals.Horse;
+import com.zipcodewilmington.froilansfarm.Housing.Farm;
+import com.zipcodewilmington.froilansfarm.Housing.Storage;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StorageTest {
+
+    @Test
+    public void testAddHorse(){
+        Horse horse = new Horse();
+        Storage stable = new Storage();
+
+        stable.addHorse(horse);
+
+        Assert.assertNotNull(stable);
+    }
+
+    @Test
+    public void testAddChicken(){
+        Chicken chicken = new Chicken();
+        Storage ChickenCoop = new Storage();
+        ChickenCoop.addChicken(chicken);
+
+        Assert.assertNotNull(ChickenCoop);
+    }
+
+    @Test
+    public void testAddPerson(){
+        Farmer fred = new Farmer();
+        Storage house = new Storage();
+
+        house.addPerson(fred);
+        Assert.assertNotNull(house);
+    }
 }
