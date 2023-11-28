60587b0fa165c407315d207c24ed3d4f91a5c380
hyunjialee <hyjialee@gmail.com>
2023-11-28 12:04:16 -0500

    fixes on schedule test

--- src/test/java/com/zipcodewilmington/froilansfarm/DailySchedule/MondayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/DailySchedule/MondayTest.java
@@ -3,8 +3,11 @@ package com.zipcodewilmington.froilansfarm.DailySchedule;
 import com.zipcodewilmington.froilansfarm.Animals.Farmer;
 import com.zipcodewilmington.froilansfarm.Animals.Horse;
 import com.zipcodewilmington.froilansfarm.Animals.Pilot;
+import com.zipcodewilmington.froilansfarm.CropsAndProduce.Crop;
 import com.zipcodewilmington.froilansfarm.CropsAndProduce.CropRow;
+import com.zipcodewilmington.froilansfarm.CropsAndProduce.TomatoPlant;
 import com.zipcodewilmington.froilansfarm.FarmEquip.CropDuster;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -18,8 +21,12 @@ public class MondayTest {
         CropDuster cropDuster = new CropDuster();
         //When
         boolean result = Froilanda.mount(cropDuster);
+
+        String expected = "flying";
+        String actual = cropDuster.fly();
         //The
         assertEquals(true, result);
+        Assert.assertEquals(expected, actual);
 
     }
 
@@ -28,7 +35,13 @@ public class MondayTest {
         //Given
         Pilot Froilanda = new Pilot();
         CropRow cropRow = new CropRow();
+
+        Crop tomato = new TomatoPlant();
+        cropRow.plant(tomato);
+
         CropDuster cropDuster = new CropDuster();
+
+        Froilanda.mount(cropDuster);
         //When
         boolean result = cropDuster.fertilize(cropRow);
         //The
